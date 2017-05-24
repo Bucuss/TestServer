9d5190cd00d2365beb92c7993e99295cee3d656a
bucuss <zzm773@gmail.com>
2017-05-24 18:05:40 +0800

    C#_server

---
+++ TestServer/BattleComBuffer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServer
+{   
+    //临时战斗指令数据
+    class BattleComBuffer
+    {
+        private int dirc;
+        private int attack;
+        private int dircContinues;
+
+        public BattleComBuffer() {
+            this.dirc = 0;
+            this.attack = 0;
+            this.dircContinues = 0;
+        }
+        public BattleComBuffer(int dirc,int attack,int dircContinues) {
+            this.dirc = dirc;
+            this.attack = attack;
+            this.dircContinues = dircContinues;
+        }
+
+        public ByteBuffer WriteAsByte() {
+            ByteBuffer bytebuffer = new ByteBuffer();
+            bytebuffer.WriteInt(dirc);
+            bytebuffer.WriteInt(attack);
+            bytebuffer.WriteInt(dircContinues);
+            return bytebuffer;
+        }
+
+        public void ReadByByte(byte[] result) {
+            ByteBuffer bytebuffer = new ByteBuffer(result);
+            this.dirc = bytebuffer.ReadInt();
+            this.attack = bytebuffer.ReadInt();
+            this.dircContinues = bytebuffer.ReadInt();
+        }
+    }
+}
--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -3,13 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
 
 namespace TestServer
 {
     class Program
     {
+        private static byte[] result = new byte[1024];
+        private const int port = 8088;
+        private static string IpStr = "127.0.0.1";
+        private static Socket serverSocket;
+
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Parse(IpStr);
+            IPEndPoint ip_end_point = new IPEndPoint(ip, port);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ip_end_point);
+            serverSocket.Listen(10);
+
+            Console.WriteLine("Start Listen {0} success", serverSocket.LocalEndPoint.ToString());
+            Thread thread = new Thread(ClientConnectListen);
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        private static void ClientConnectListen() {
+            while (true) {
+                Socket clientSocket = serverSocket.Accept();
+                Console.WriteLine("Client {0} load success", clientSocket.RemoteEndPoint.ToString());
+                ByteBuffer buffer = new ByteBuffer();
+                buffer.WriteString("Connected Server");//return data
+                clientSocket.Send(WriteMessage(buffer.ToBytes()));
+                Thread thread = new Thread(RecieveMessage);
+                thread.Start(clientSocket);
+            }
+
+        }
+
+        private static byte[] WriteMessage(byte[] message)
+        {
+            MemoryStream ms = null;
+            //using 强制资源清理
+            using (ms = new MemoryStream())
+            {
+                ms.Position = 0;
+                BinaryWriter writer = new BinaryWriter(ms);
+                ushort msglen = (ushort)message.Length;
+                writer.Write(msglen);
+                writer.Write(message);
+                writer.Flush();
+                return ms.ToArray();
+            }
+        }
+
+        private static void RecieveMessage(object clientSocket) {
+            Socket mClientSocket = (Socket)clientSocket;
+            while (true) {
+                try {
+                    int receiveNumber = mClientSocket.Receive(result);
+                    Console.WriteLine("Receive Client {0} massage , length is {1}", mClientSocket.RemoteEndPoint.ToString(), receiveNumber);
+                    ByteBuffer buff = new ByteBuffer(result);
+
+                    int len = buff.ReadShort();
+                    String data = buff.ReadString();
+                    Console.WriteLine("Data: {0}", data);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    mClientSocket.Shutdown(SocketShutdown.Both);
+                    mClientSocket.Close();
+                    break;
+                }
+            }
         }
     }
 }
